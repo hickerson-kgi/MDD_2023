b8481d9a7568f1a7a681266cea93f002a8b33517
connornieh <138164612+connornieh@users.noreply.github.com>
2023-07-05 11:00:22 -0700

    Revert "mssg"
    This reverts commit dd01aa4ea5e7089b36df6959cae3f763a7f93e99.

--- testing.py
+++ testing.py
@@ -1,6 +1,4 @@
 import random
-
-
 def generate_equation():
     operators = ['+', '-', '*', '/']
     operator = random.choice(operators)
@@ -10,16 +8,10 @@ def generate_equation():
     equation = f"{num1} {operator} {num2}"
     result = eval(equation)
     return equation, result
-
-
 equations = []
 for _ in range(4):
     equation, result = generate_equation()
-    equations.append(equation)
-    equations.append(result)
-
-# Print the list with alternating equation and result
-for i in range(0, len(equations), 2):
-    equation = equations[i]
-    result = equations[i + 1]
-    print (equations)
+    equations.append((equation, result))
+# Print the list of equations and their results
+for equation, result in equations:
+    print({equation}, {result})
