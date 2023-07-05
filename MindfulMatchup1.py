21de0ee72aa0c2a72ef83c950e6e88b59862262a
ajoba002 <ajoba002@ucr.edu>
2023-07-05 10:55:47 -0700

    mssg

--- MindfulMatchup1.py
+++ MindfulMatchup1.py
@@ -15,6 +15,8 @@ from kivy.uix.gridlayout import GridLayout
 import random
 
 import random
+
+
 def generate_equation():
     operators = ['+', '-', '*', '/']
     operator = random.choice(operators)
@@ -24,14 +26,19 @@ def generate_equation():
     equation = f"{num1} {operator} {num2}"
     result = eval(equation)
     return equation, result
+
+
 equations = []
 for _ in range(4):
     equation, result = generate_equation()
-    equations.append((equation, result))
-# Print the list of equations and their results
-for equation, result in equations:
-    print({equation}, {result})
+    equations.append(equation)
+    equations.append(result)
 
+# Print the list with alternating equation and result
+for i in range(0, len(equations), 2):
+    equation = equations[i]
+    result = equations[i + 1]
+    print (equations)
 
 def answer_equation(equation):
     # actually figure out the answer
