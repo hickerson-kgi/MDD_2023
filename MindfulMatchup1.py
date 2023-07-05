7a2fc1227c0e0875274b783732383f52b3dc2393
ajoba002 <ajoba002@ucr.edu>
2023-07-05 09:55:28 -0700

    mssg

--- MindfulMatchup1.py
+++ MindfulMatchup1.py
@@ -10,6 +10,24 @@ from kivy.uix.gridlayout import GridLayout
  
 # This class stores the info of .kv file
 # when it is called goes to my.kv file
+
+#math equations
+import random
+def generate_equation():
+    operators = ['+', '-', '*', '/']
+    operator = random.choice(operators)
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+
+    equation = f"{num1} {operator} {num2}"
+    return equation
+
+# Example usage:
+equation = generate_equation()
+print("Generated equation:", equation)
+result = eval(equation)
+print("Result:", result)
+
 class MainWidget(GridLayout):
     pass
  
