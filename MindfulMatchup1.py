50910264032781b5bd29d8fd8f9882d51dcfd859
ajoba002 <ajoba002@ucr.edu>
2023-07-06 13:57:42 -0700

    mssg

--- MindfulMatchup1.py
+++ MindfulMatchup1.py
@@ -1,11 +1,10 @@
-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 import random
 from kivy.graphics import Color
 from kivy.clock import Clock
 
-
+# Function to generate random arithmetic equations
 def generate_equation():
     questions = []
     answers = []
@@ -28,6 +27,7 @@ def generate_equation():
     return questions, answers
 
 
+# MainWidget represents the main widget of the application
 class MainWidget(GridLayout):
 
     def __init__(self, **kwargs):
@@ -40,11 +40,13 @@ class MainWidget(GridLayout):
         left_ids = ['1a_btn', '1b_btn', '1c_btn', '1d_btn']
         for i in range(len(left_ids)):
             self.ids[left_ids[i]].text = str(self.questions[i])
+
         right_ids = ['2a_btn', '2b_btn', '2c_btn', '2d_btn']
         random.shuffle(answer_order)
         for j in range(len(right_ids)):
             self.ids[right_ids[j]].text = str(self.answers[answer_order[j]])
 
+    # Function to handle button click events
     def generate(self, id):
         self.selected_question = None
         self.selected_answer = None
@@ -70,15 +72,18 @@ class MainWidget(GridLayout):
             self.selected_answer.background_color = (1, 0, 0, 1)
             Clock.schedule_once(self.reset_colors, 1)  # Reset colors after 1 second
 
+    # Function to reset button colors after an incorrect match
     def reset_colors(self, dt):
             self.selected_question.background_color = (1, 1, 1, 1)
             self.selected_answer.background_color = (1, 1, 1, 1)
             self.selected_question = None
             self.selected_answer = None
 
+    # Function to handle button click events on answer buttons
     def answer(self, id):
         self.ids[id].background_color = (1, 1, 1, 1)
 
+    # Function to restart the game by generating new equations
     def restart_game(self):
         questions, answers = generate_equation()
         answer_order = [0, 1, 2, 3]
@@ -96,13 +101,13 @@ class MainWidget(GridLayout):
             btn.text = str(answers[answer_order[j]])
             btn.background_color = (0.5, 0.5, 0.5, 1)
 
-        MainWidget.prev_btn = None
 
-
+# MindfulMatchup1App represents the Kivy application
 class MindfulMatchup1App(App):
     def build(self):
         return MainWidget()
 
 
+# Run the Kivy application
 if __name__ == '__main__':
     MindfulMatchup1App().run()
