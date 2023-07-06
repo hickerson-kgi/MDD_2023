bde0e3cb90ddfcd547e81595bad08ed6bc12f0cf
connornieh <138164612+connornieh@users.noreply.github.com>
2023-07-06 11:28:32 -0700

    color matching

--- MindfulMatchup1.py
+++ MindfulMatchup1.py
@@ -2,6 +2,8 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 import random
+from kivy.graphics import Color
+from kivy.clock import Clock
 
 
 def generate_equation():
@@ -22,43 +24,51 @@ def generate_equation():
 
 
 class MainWidget(GridLayout):
-    prev_btn = None  # Initialize prev_btn with None
 
     def __init__(self, **kwargs):
-        super(MainWidget, self).__init__(**kwargs)
-        self.questions, self.answers = generate_equation()
-        answer_order = [0, 1, 2, 3]
-
-        left_ids = ['a1_btn', 'b1_btn', 'c1_btn', 'd1_btn']
-        for i in range(len(left_ids)):
-            self.ids[left_ids[i]].text = str(questions[i])
-        right_ids = ['a2_btn', 'b2_btn', 'c2_btn', 'd2_btn']
-        random.shuffle(answer_order)
-        for j in range(len(right_ids)):
-            self.ids[right_ids[j]].text = str(answers[answer_order[j]])
-
-    def generate(self, id):
-
-        if self.ids[id].
-
         self.selected_question = None  # Store the currently selected equation button
         self.selected_answer = None  # Store the currently selected solution button
         self.matching_colors = []  # Store random matching colors for button pairs
 
+        super(MainWidget, self).__init__(**kwargs)
+        self.questions, self.answers = generate_equation()
+        answer_order = [0, 1, 2, 3]
+
+        left_ids = ['1a_btn', '1b_btn', '1c_btn', '1d_btn']
+        for i in range(len(left_ids)):
+            self.ids[left_ids[i]].text = str(self.questions[i])
+        right_ids = ['2a_btn', '2b_btn', '2c_btn', '2d_btn']
+        random.shuffle(answer_order)
+        for j in range(len(right_ids)):
+            self.ids[right_ids[j]].text = str(self.answers[answer_order[j]])
+
+    def generate(self, id):
+        if id.startswith("1"):
+            self.selected_question = id
+        else:
+            self.selected_answer = id
+
         self.matching_colors = [Color(random.random(), random.random(), random.random(), 1) for _ in
-                                range(len(equations))]
+                                range(len(self.questions))]
 
-        btn = self.ids[id]
-        btn_text = btn.text
-        btn_index = self.questions.index(btn)
-        if prev_btn_index == btn_index:
-            prev_btn = MainWidget.prev_btn
-            prev_btn_index = self.answers.index(prev_btn)
+        if self.questions.index(self.selected_question.text) == self.answers.index(self.selected_answer.text):
+            matching_color_equation = self.matching_colors[self.questions.index(self.selected_question.text)]
+            self.selected_equation.background_color = matching_color_equation.rgba
+            matching_color_solution = self.matching_colors[self.answers.index(self.selected_answer.text)]
+            self.selected_solution.background_color = matching_color_solution.rgba
+            self.selected_equation = None
+            self.selected_solution = None
+        else:
+            # Incorrect match
+            self.selected_question.background_color = (1, 0, 0, 1)
+            self.selected_answer.background_color = (1, 0, 0, 1)
+            Clock.schedule_once(self.reset_colors, 1)  # Reset colors after 1 second
 
-            if prev_btn_index == btn_index:
-                prev_btn.background_color = (0, 1, 0.5, 1)
-        btn.background_color = (0, 1, 0.5, 1)
-        #MainWidget.prev_btn = btn
+    def reset_colors(self, dt):
+            self.selected_equation.background_color = (1, 1, 1, 1)
+            self.selected_solution.background_color = (1, 1, 1, 1)
+            self.selected_equation = None
+            self.selected_solution = None
 
     def answer(self, id):
         self.ids[id].background_color = (1, 1, 1, 1)
