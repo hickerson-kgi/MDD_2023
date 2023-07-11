98b280ba36089aa60a011d80ea9db506392e812d
1overInf <armandogfelix76@gmail.com>
2023-07-11 15:47:17 -0700

    Timer reset with reset button and tally for incorrect

--- RasPi_TestCode.py
+++ RasPi_TestCode.py
@@ -11,16 +11,24 @@ button_pin2= 26
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button_pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
 class LightingScreen(Screen):
     def __init__(self, **kwargs):
         super(LightingScreen, self).__init__(**kwargs)
-        self.button = Button(text="Screen Lighting")
-        self.button.bind(on_press=self.on_button_press)
-        self.add_widget(self.button)
 
-    def on_button_press(self, instance):
-        self.button.text = "Button Pressed"
+        self.button1 = Button(text="Button 1")
+        self.button1.bind(on_press=self.on_button1_press)
+        self.add_widget(self.button1)
 
+        self.button2 = Button(text="Button 2")
+        self.button2.bind(on_press=self.on_button2_press)
+        self.add_widget(self.button2)
+
+    def on_button1_press(self, instance):
+        self.button1.text = "Button 1 Pressed"
+
+    def on_button2_press(self, instance):
+        self.button2.text = "Button 2 Pressed"
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
