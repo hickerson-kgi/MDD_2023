0716d9ed983adf3209c15bd96aa9a58578ef0d15
celinaibelaidene <cibel001@ucr.edu>
2023-07-11 14:37:14 -0700

    hiiii

--- RasPi_TestCode.py
+++ RasPi_TestCode.py
@@ -1,6 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 import RPi.GPIO as GPIO
 
@@ -10,9 +10,10 @@ button_pin = 12
 
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-class LightingScreen(Screen):
+class LightingGrid(GridLayout):
     def __init__(self, **kwargs):
-        super(LightingScreen, self).__init__(**kwargs)
+        super(LightingGrid, self).__init__(**kwargs)
+        self.cols = 1
         self.button = Button(text="Screen Lighting")
         self.button.bind(on_press=self.on_button_press)
         self.add_widget(self.button)
@@ -22,13 +23,11 @@ class LightingScreen(Screen):
 
 class MyApp(App):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(LightingScreen(name="lighting"))
-        return sm
+        return LightingGrid()
 
 def on_pushbutton_press(channel):
     if GPIO.input(button_pin) == GPIO.LOW:
-        app.root.get_screen("lighting").on_button_press(None)
+        app.root.on_button_press(None)
 
 GPIO.add_event_detect(button_pin, GPIO.BOTH, callback=on_pushbutton_press, bouncetime=300)
 
